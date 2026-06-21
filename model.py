70ed84d4b3080130ed19d118456c199a43850446
rashedsumon <rashedsumon57@gmail.com>
2026-06-21 23:48:10 +0600

    Update model.py

--- model.py
+++ model.py
@@ -18,8 +18,8 @@ def train_pipeline():
     
     # 2. Split Data
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_test_size=0.2, random_state=42, stratify=y
-    )
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
     
     # 3. Scale Features (Crucial for medical biomechanical metrics)
     scaler = StandardScaler()
